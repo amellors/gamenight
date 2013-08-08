8331bac10c27ded807aaddc33006f6a306b3ffb0
Andy Mellors <amellors@gmail.com>
2013-08-08 09:16:38 -0400

    Small fix for players relation

--- app/models/gameplay.rb
+++ app/models/gameplay.rb
@@ -2,5 +2,5 @@ class Gameplay < ActiveRecord::Base
   belongs_to :night
   belongs_to :game
   
-  has_and_belongs_to_many :winners, class_name: "Player", join_table: "gameplay_winners"
+  has_and_belongs_to_many :winners, class_name: :Player, join_table: "gameplay_winners"
 end

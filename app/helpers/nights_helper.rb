8f1747a17a82342a521eb7099f4c3e098d35a52a
Andy Mellors <amellors@gmail.com>
2013-08-10 08:26:35 -0400

    Made a more pretty landing page

--- app/helpers/nights_helper.rb
+++ app/helpers/nights_helper.rb
@@ -1,2 +1,8 @@
 module NightsHelper
+  def finishedCount
+    tempCount = 0
+    gameplays.each do |gp|
+      tempCount += 1 if gp.finished
+    end
+  end
 end
--- app/models/night.rb
+++ app/models/night.rb
@@ -11,4 +11,12 @@ class Night < ActiveRecord::Base
       errors.add(:players, "must have more than #{TextHelper.pluralize player_ids.count, 'player'}")
     end
   end
+  
+  def finishedCount
+    tempCount = 0
+    gameplays.each do |gp|
+      tempCount = tempCount + 1 if gp.finished
+    end
+  end
+  
 end

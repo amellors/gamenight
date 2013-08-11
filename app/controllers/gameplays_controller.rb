1e7b6c9b7dd982c1312d86cf3448c3e39c592825
Andy Mellors <amellors@gmail.com>
2013-08-11 14:29:12 -0400

    starting to work on AJAX... added ability to delete gameplays

--- app/controllers/gameplays_controller.rb
+++ app/controllers/gameplays_controller.rb
@@ -1,7 +1,22 @@
 class GameplaysController < ApplicationController
   def create
     @night = Night.find(params[:night_id])
-    @gameplay = @night.gameplays.create(params[:gameplay].permit(:game_id,:status,:notes,:finished,:winner_ids => []))
-    redirect_to night_path(@night)
+    @gameplay = @night.gameplays.build(params[:gameplay].permit(:game_id,:status,:notes,:finished,:winner_ids => []))
+    respond_to do |format|
+      if @gameplay.save
+        format.html { redirect_to(@night, notice: 'Gameplay successfully created.') }
+        format.json { render action: 'show', status: :created, location: @gameplay }
+      else
+        format.html { redirect_to(@night, notice: 'Gameplay could not be created.') }
+        format.json { render json: @gameplay.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def destroy
+    night = Night.find(params[:night_id])
+    night.gameplays.destroy(Gameplay.find(params[:id]))
+
+    redirect_to night_url(night)
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,6 @@
 class WelcomeController < ApplicationController
   def index
+    @nights = Night.all.order("date ASC")
+    @games = Game.all
   end
 end

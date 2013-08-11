a4c9356a42a4d0df38c7db42e09bd5ec4efb0cda
Andy Mellors <amellors@gmail.com>
2013-08-11 09:39:13 -0400

    Playing with game display

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,4 +2,13 @@ class Game < ActiveRecord::Base
   validates :name, presence: true, uniqueness: true
   
   has_many :gameplays
+  
+    def finishedCount
+    tempCount = 0
+    gameplays.each do |gp|
+      tempCount += 1 if gp.finished
+    end
+    tempCount
+  end
+  
 end
--- test/controllers/welcome_controller_test.rb
+++ test/controllers/welcome_controller_test.rb
@@ -4,6 +4,10 @@ class WelcomeControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', minimum: 4
+    assert_select '#main .entry', 3
+    assert_select 'h3', '2013-01-01'
+    assert_select '.count', /\d+\s+\(\d+\)/
   end
 
 end

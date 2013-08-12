e350316aeba7e29ff4a6c8b73665be8f01dff47c
Andy Mellors <amellors@gmail.com>
2013-08-11 20:53:56 -0400

    Made the gameplay delete all kinds of AJAXy

--- app/controllers/gameplays_controller.rb
+++ app/controllers/gameplays_controller.rb
@@ -1,25 +1,26 @@
 class GameplaysController < ApplicationController
   def create
-    @night = Night.find(params[:night_id])
-    @gameplay = @night.gameplays.build(params[:gameplay].permit(:game_id,:status,:notes,:finished,:winner_ids => []))
+    night = Night.find(params[:night_id])
+    gameplay = night.gameplays.build(params[:gameplay].permit(:game_id,:status,:notes,:finished,:winner_ids => []))
     respond_to do |format|
-      if @gameplay.save
-        format.html { redirect_to(@night, notice: 'Gameplay successfully created.') }
-        format.json { render action: 'show', status: :created, location: @gameplay }
+      if gameplay.save
+        format.html { redirect_to(night, notice: 'Gameplay successfully created.') }
+        format.json { render action: 'show', status: :created, location: gameplay }
       else
-        format.html { redirect_to(@night, notice: 'Gameplay could not be created.') }
-        format.json { render json: @gameplay.errors, status: :unprocessable_entity }
+        format.html { redirect_to(night, notice: 'Gameplay could not be created.') }
+        format.json { render json: gameplay.errors, status: :unprocessable_entity }
       end
     end
   end
   
   def destroy
-    night = Night.find(params[:night_id])
-    gp_name = Gameplay.find(params[:id]).game.name
-    night.gameplays.destroy(Gameplay.find(params[:id]))
+    @night = Night.find(params[:night_id])
+    @gp_name = Gameplay.find(params[:id]).game.name
+    @night.gameplays.destroy(Gameplay.find(params[:id]))
 
     respond_to do |format|
-      format.html { redirect_to(night, notice: "Gameplay for #{gp_name} successfully removed.") }
+      format.html { redirect_to(@night, notice: "Gameplay for #{@gp_name} successfully removed.") }
+      format.js
     end
   end
 end

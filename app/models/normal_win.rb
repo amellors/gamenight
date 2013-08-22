2219a2b1d54a59cb365d314c5f45896143e7ad77
Andy Mellors <amellors@gmail.com>
2013-08-22 14:14:20 -0400

    Adding Normal Win info

--- app/models/normal_win.rb
+++ app/models/normal_win.rb
@@ -1,4 +1,10 @@
 class NormalWin < ActiveRecord::Base
   has_one :gameplay, :as => :win_info
   belongs_to :winner, class_name: :Player, foreign_key: "winner_id"
+
+  def to_partial_path() "win/normal_win" end
+  
+  def self.win_params (params)
+    params.require(:win_info).permit(:winner_id)
+  end
 end

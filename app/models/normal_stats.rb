e4df5bd869b2b5fedbf4ddc341880cc0a1a122c4
Andy Mellors <amellors@gmail.com>
2013-08-30 15:13:55 -0400

    Added Graph for Normal Games

--- app/models/normal_stats.rb
+++ app/models/normal_stats.rb
@@ -2,6 +2,23 @@ class NormalStats
 
   def initialize (wins)
     @wins = wins
+    @winner_count = Hash.new
+    @wins.each do |win|
+      if @winner_count[win.winner.name]
+        @winner_count[win.winner.name] += 1
+      else
+        @winner_count[win.winner.name] = 1
+      end
+    end
+    # @winner_count = @winner_count.sort {|a,b| b[1]<=>a[1]}
+  end
+  
+  def players
+    @winner_count.map { |each| each[0] }
+  end
+  
+  def scores
+    @winner_count.map { |each| each[1] }
   end
   
   def partial_name

d71facaa9c5cb2735637fdfa232604bcd464b1b1
Andy Mellors <amellors@gmail.com>
2013-08-31 11:41:47 -0400

    Coop win stats & graph

--- app/models/coop_stats.rb
+++ app/models/coop_stats.rb
@@ -1,7 +1,34 @@
 class CoopStats
 
   def initialize (wins)
-    @wins = wins
+    @game_wins = 0
+    @player_wins = 0
+    @traitor_wins = 0
+    @scores = Array.new
+    wins.each do |win|
+      @scores << win.players_score
+      if win.players_win
+        @player_wins += 1 
+      else
+        @game_wins += 1
+      end
+    end 
+  end
+
+  def game_wins
+    @game_wins
+  end
+  
+  def player_wins
+    @player_wins
+  end
+  
+  def scores
+    @scores
+  end
+  
+  def high_score
+    @scores[@scores.index(@scores.compact.max)]
   end
   
   def partial_name

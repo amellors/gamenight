1885e82fd44922c7be43363bf8fc02b91bbf28b8
Andy Mellors <amellors@gmail.com>
2013-09-10 11:56:36 -0400

    Added helper for filtering

--- app/helpers/filter_helper.rb
+++ app/helpers/filter_helper.rb
@@ -1,2 +1,12 @@
 module FilterHelper
+  def filter_links
+    case session[:filter]
+    when "Tuesday"
+      link_to('All', all_path) + ' | Tuesday | ' + link_to('Cape', cape_path)
+    when "Cape"
+      link_to('All', all_path) + ' | ' + link_to('Tuesday', tuesday_path) + ' | Cape'
+    else
+      link_to('Tuesday', tuesday_path) + ' | ' + link_to('Cape', cape_path) 
+    end
+  end
 end

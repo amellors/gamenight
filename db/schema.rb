7187e0d0e8688b5660346e16a49c05f70400f5f9
Andrew Mellors <amellors@gmail.com>
2013-12-30 22:11:29 -0500

    Add Night Types

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130909185102) do
+ActiveRecord::Schema.define(version: 20131231021107) do
 
   create_table "coop_wins", force: true do |t|
     t.boolean  "players_win"
